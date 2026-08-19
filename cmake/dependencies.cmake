b534f0310371f7f53f2e8d99a313a8080a8fd468
Radu Iulian <iulianradu21@outlook.com>
2026-08-19 16:17:12 +0300

    Added libgpiod dependency

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,6 +1,8 @@
 function(setup_asio)
     include(FetchContent)
+    set(FETCHCONTENT_QUIET ON)
 
+    message (STATUS "Setting asio up...")
     FetchContent_Declare(
         asio_source
         URL https://sourceforge.net/projects/asio/files/asio/1.38.2%20%28Stable%29/asio-1.38.2.tar.gz/download
@@ -14,13 +16,17 @@ function(setup_asio)
 
     add_library(asio INTERFACE)
     target_include_directories(asio INTERFACE ${asio_source_SOURCE_DIR}/include)
+    
+    set(FETCHCONTENT_QUIET OFF)
+    message (STATUS "Setting asio up...done")
 
 endfunction()
 
 function(setup_crow)
-
     include(FetchContent)
+    set(FETCHCONTENT_QUIET ON)
 
+    message (STATUS "Setting crow up...")
     FetchContent_Declare(
         crow_source
         GIT_REPOSITORY https://github.com/CrowCpp/Crow.git
@@ -32,9 +38,33 @@ function(setup_crow)
     add_library(crow INTERFACE)
     target_include_directories(crow INTERFACE ${crow_source_SOURCE_DIR}/include)
 
+    set(FETCHCONTENT_QUIET OFF)
+    message (STATUS "Setting crow up...done")
+
+endfunction()
+
+function(setup_libgpiod)
+    message (STATUS "Setting libgpiod up...")
+
+    execute_process(
+        COMMAND bash ${CMAKE_SOURCE_DIR}/scripts/install_libgpiod.sh
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        OUTPUT_QUIET
+        ENVIRONMENT
+            MESON_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/toolchain/arm-linux-gnueabihf.ini
+    )
+
+    message(STATUS "Adding libgpod to pkgconfig path")
+    set(ENV{PKG_CONFIG_PATH}
+        "${CMAKE_BINARY_DIR}/libgpiod/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}"
+    )
+
+    message (STATUS "Setting libgpiod up...done")
+
 endfunction()
 
 function(setup_dependencies)
     setup_asio()
     setup_crow()
+    setup_libgpiod()
 endfunction()
---
+++ scripts/install_libgpiod.sh
@@ -0,0 +1,25 @@
+readonly LIB=libgpiod
+readonly VERSION=2.3
+readonly CMAKE_BUILD_ROOT=$(pwd)
+readonly TARGET_LIB=$LIB-$VERSION.tar.xz
+readonly SOURCE_PATH=$CMAKE_BUILD_ROOT/$LIB/libgpiod-$VERSION
+readonly BUILD_PATH=$CMAKE_BUILD_ROOT/$LIB/build
+readonly INSTALL_PATH=$CMAKE_BUILD_ROOT/$LIB
+
+mkdir -p $SOURCE_PATH
+
+if [[ ! -f  $TARGET_LIB ]]; then
+    echo "Downloading libgpio $CMAKE_C_COMPILER $CC"
+    wget --quiet https://mirrors.edge.kernel.org/pub/software/libs/libgpiod/$TARGET_LIB
+fi
+
+
+cd $LIB
+
+tar xf $CMAKE_BUILD_ROOT/$TARGET_LIB
+
+mkdir -p $BUILD_PATH && cd $BUILD_PATH
+meson setup --prefix=$INSTALL_PATH --buildtype=release $SOURCE_PATH --cross-file=$MESON_TOOLCHAIN_FILE -Dtests=disabled
+echo "Building $LIB-$VERSION..."
+ninja
+ninja install
